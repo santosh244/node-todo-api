9fbf7ca0b7c8b831e518ff6fb6a2ecb70bc985d7
Santosh <skskumar448@gmail.com>
2019-02-11 21:49:04 +0530

    get api

--- server/server.js
+++ server/server.js
@@ -39,17 +39,12 @@ app.post('/user', (req, res) => {
 module.exports ={app}
 
 
-
-app.post('/todos', (req, res) => {
- var todo = new Todo({
-   text: req.body.text
- });
-
- todo.save().then((doc) => {
-   res.send(doc);
- }, (e) => {
-   res.status(400).send(e);
- });
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send(todos);
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
 
